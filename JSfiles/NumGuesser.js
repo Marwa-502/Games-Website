037b59ac5ba95fba163b9b3ab563f2ab61b9b119
Bismillahi Ar Rahmani Ar Rahim <bismillah@Bismillahis-MacBook-Air.local>
2023-10-30 13:35:28 -0700

    updated nav bars across all pages, created Number-Guesser-Game interface, fixed backgroun issue

--- JSfiles/NumGuesser.js
+++ JSfiles/NumGuesser.js
@@ -1,33 +1,86 @@
-const prompt = require('prompt-sync')(); // library for handling user input
 
-//Issues i had with this code were as follows:
-// couldnt get the game to ask the user to keep trying : answer was i needed a while loop
-// couldnt figure out why it would keep looping after player guessed too high : answer i needed to put in a break
-// couldnt figure out why it did the same looping after player guessed too low even after putting in the break. : 
-// .....: answer was i needed to put line 16 and 17 INSIDE the While loop. it was originally above it.
+const playerGuessInput = document.getElementById("userGuess");
+const submitGuessButton = document.getElementById("submitGuess");
+const messageElement = document.getElementById("message");
+const playAgainButton = document.getElementById("playAgain");
+const timerElement = document.getElementById("timer");
+const timeLeftElement = document.getElementById("timeLeft");
 
+let interval; // Declare the interval variable
+let numGuesses = 5;
 
-const randomNum = Math.round(Math.random() * 100)
-
-let player = "";
-
-while (true) {
-    let playerGuess = Number(prompt("Guess a number between 0 and 100: "));
-    console.log("You chose", playerGuess);
+function checkGuess() {
+    const playerGuess = parseInt(playerGuessInput.value); // Saving the user's guess
 
     if (playerGuess === randomNum) {
-        console.log("Awesome Guess! The number is: ", playerGuess,);
-        console.log("You WIN!");
-        break;
+        messageElement.textContent = "Congrats!! You Guessed Right!";
+        endGame();
+    } else if (numGuesses === 1) {
+        messageElement.textContent = `Womp, Womp! You're Out Of Tries! The number was ${randomNum}`;
+        endGame();
     } else {
-        if (playerGuess < 0 || playerGuess > 100) {
-            console.log("You chose: ,", userGuess, "Try again! pick a number between 0-100");
-
-        } else if (playerGuess > randomNum) {
-            console.log("Your guess of, ", playerGuess, "is too high, please try again!")
-        }
-        if (playerGuess < randomNum) {
-            console.log("Your guess of, ", playerGuess, "is too low, please try again!")
-        }
+        messageElement.textContent = `Wrong! Try Again. You have ${numGuesses - 1} Guesses Left!`;
+        numGuesses--;
     }
+    playerGuessInput.value = "";
 }
+
+function endGame() {
+    submitGuessButton.disabled = true;
+}
+
+function countdown(seconds) {
+    let counter = seconds;
+
+    interval = setInterval(() => {
+        if (counter > 0) {
+            counter--;
+            timeLeftElement.textContent = counter;
+        } else {
+            messageElement.textContent = `TIME'S UP! The correct number was ${randomNum}.`;
+            endGame();
+        }  
+    }, 1000);
+}
+
+function startGame() {
+    numGuesses = 5;
+    randomNum = Math.floor(Math.random() * 25);
+    submitGuessButton.disabled = false;
+    messageElement.textContent = "";
+}
+
+// Button events
+submitGuessButton.addEventListener("click", checkGuess);
+playAgainButton.addEventListener("click", () => location.reload());
+
+// Start the game when the page loads
+countdown(60);
+startGame();
+
+
+
+// const prompt = require('prompt-sync')();
+
+// while (true) {
+//     let playerGuess = Number(prompt("Guess a number between 0 and 100: "));
+//     console.log("You chose", playerGuess);
+
+//     if (playerGuess === randomNum) {
+//         console.log("Awesome Guess! The number is: ", playerGuess,);
+//         console.log("You WIN!");
+//         break;
+//     } else {
+//         if (playerGuess < 0 || playerGuess > 100) {
+//             console.log("You chose: ,", userGuess, "Try again! pick a number between 0-100");
+
+//         } else if (playerGuess > randomNum) {
+//             console.log("Your guess of, ", playerGuess, "is too high, please try again!")
+//         }
+//         if (playerGuess < randomNum) {
+//             console.log("Your guess of, ", playerGuess, "is too low, please try again!")
+//         }
+//     }
+// }
+
+// }
