d4c667102ab214ea560edde0b6f4a3847faa4c3a
Novita H <novitahamilton@gmail.com>
2023-11-14 09:41:48 -0800

    Rock Paper Scissors Addition (1) Update RPSgame.html : remove previous classes and added entirely new classes to remove styling (2) Update RPS.js files with entirely new code (3) Update RPS styling in style.css

--- JSfiles/RPSGame.js
+++ JSfiles/RPSGame.js
@@ -1,70 +1,134 @@
-const prompt = require('prompt-sync')();
+'use strict';
+// Global Variables
+let play, userChoice, botChoice, roundsLeft, userScore, botScore;
+const winRoundPoints = 10; // Points earned if win a round
+const playerOption = ['rock', 'paper', 'scissors'];
 
-function getUserChoice() {
+// Selecting elements
+const elementBtnPlay = document.querySelector('.btn-play');
+const elementChooseWeapon = document.querySelector('.choose-weapon');
+const elementWeaponOpt = document.querySelectorAll('.weapon-option');
+const elementUserChoice = document.querySelector('.user-choice');
+const elementBotChoice = document.querySelector('.bot-choice');
+const elementRound = document.querySelector('.round');
+const elementUserScore = document.querySelector('.user-score-1');
+const elementBotScore = document.querySelector('.bot-score');
+const elementBtnReset = document.querySelector('.btn-reset');
 
-    return prompt("Choose Rock, Paper, or Scissors: ").toLowerCase();
+// Event handlers
+window.addEventListener('load', init);
+elementBtnPlay.addEventListener('click', playMode); // Change Play to Round, Show reset button and bot choice
+elementBtnReset.addEventListener('click', init);
+
+// Main functions
+
+// *Initial state*
+function init() {
+  play = true;
+  roundsLeft = 5;
+  userScore = 0;
+  botScore = 0;
+  userChoice = 'user';
+  displayUserChoice(userChoice); // to reset user-choice display
+  botChoice = 'bot';
+  displayBotChoice(botChoice);
+
+  getUserChoice();
+  hideElement(elementBtnReset);
+  hideElement(elementRound);
 }
 
-function getComputerChoice() {
-
-    const randomNum = Math.floor(Math.random() * 3);
-    if (randomNum == 0) {
-        return "rock";
-    } else if (randomNum == 1) {
-        return "paper";
-    } else {
-        return "scissors";
-    }
-}
-
-function determineRoundWinner(userChoice, computerChoice) {
-    console.log("You chose: ", userChoice);
-    console.log("Computer chose: ", computerChoice);
-
-    if (userChoice == computerChoice) {
-        console.log("It's a tie!");
-    } else if (
-        (userChoice == "rock" && computerChoice == "scissors") ||
-        (userChoice == "paper" && computerChoice == "rock") ||
-        (userChoice == "scissors" && computerChoice == "paper")
-    ) {
-        userScore += 1;
-        console.log("User wins the round!");
-    } else {
-        computerScore += 1;
-        console.log("Computer wins the round!");
-    }
-    console.log(`Current Scores: User: ${userScore}, Computer: ${computerScore}`);
-    console.log('---------------------------------------');
-}
-
-function printFinalScores() {
-    console.log(`Final Scores: User: ${userScore}, Computer: ${computerScore}`);
-    console.log('---------------------------------------');
-}
-
-let play = true;
-
-while (play) {
-    let roundsLeft = 5;
-    let userScore = 0;
-    let computerScore = 0;
+function playMode() {
+  while (play) {
+    showElement(elementBtnReset);
+    showElement(elementRound);
 
     while (roundsLeft > 0) {
-        const userInput = getUserChoice();
-        const computerChoice = getComputerChoice();
+      botChoice = getBotChoice();
+      console.log(botChoice);
 
-        determineRoundWinner(userInput, computerChoice);
-
-        roundsLeft -= 1;
+      // Print the choices
+      displayUserChoice(userChoice);
+      displayBotChoice(botChoice);
+      if (userChoice === botChoice) {
+        console.log(`It's a tie!`);
+      } else if (
+        (userChoice === 'rock' && botChoice === 'scissors') ||
+        (userChoice === 'paper' && botChoice === 'rock') ||
+        (userChoice === 'scissors' && botChoice === 'paper')
+      ) {
+        console.log('You WIN!'); // Display winning message
+        debugger;
+        userScore = userScore + winRoundPoints;
+        console.log(userScore); // Add 10 to userScore
+        displayElementContent(elementUserScore, userScore); // Display updated userScore
+      } else {
+        //   // Ask user if they want to keep playing
+        //   // Yes - continue
+        //   // No - exit play = false
+        //   // Restart
+        console.log('You LOSE!');
+        botScore = botScore + winRoundPoints;
+        displayElementContent(elementBotScore, botScore);
+      }
+      roundsLeft--;
+      console.log(roundsLeft);
     }
 
-    printFinalScores();
+    if (userScore === botScore) {
+      console.log(`It's a TIE!`);
+    } else
+      userScore > botScore
+        ? console.log('You WIN the game!🎉')
+        : console.log('You LOSE the game!🎉');
 
-    const replay = prompt("Do you want to play again (yes/no)?: ").toLowerCase();
+    //   Display Final Score
+    console.log('User score:', userScore);
+    console.log('Bot score:', botScore);
+    // Ask if user wants to play again
+    // const replay = prompt('Do you want to play again? (yes/no)').toLowerCase();
+    // console.log(replay);
 
-    if (replay !== "yes") {
-        console.log("Thanks for playing :)");
-        play = false;
-    }
+    // if (replay !== 'yes') {
+    //   play = false;
+    // }
+  }
 }
+
+// Display element content
+function displayElementContent(elementSelector, text) {
+  document.querySelector(elementSelector).textContent = text;
+}
+
+function showElement(elementSelector) {
+  return elementSelector.classList.remove('hidden');
+}
+
+function hideElement(elementSelector) {
+  return elementSelector.classList.add('hidden');
+}
+
+// Generate random bot choice
+function getBotChoice() {
+  const randomIndex = Math.floor(Math.random() * playerOption.length);
+  return playerOption[randomIndex];
+}
+
+function displayUserChoice(userChoice) {
+  return (elementUserChoice.src = `../images/${userChoice}.png`);
+}
+
+function displayBotChoice(botChoice) {
+  return (elementBotChoice.src = `../images/${botChoice}.png`);
+}
+
+function getUserChoice() {
+  elementWeaponOpt.forEach(function (element) {
+    element.addEventListener('click', function (event) {
+      userChoice = event.target.id;
+      console.log(userChoice);
+    });
+  });
+}
+
+function updateRound() {}
---
+++ tasks.md
@@ -0,0 +1,35 @@
+### Additional Features/Items:
+
+- [ ] Add Rock Paper Scissors game
+- [ ] Add Hangman game
+- [ ] Make site responsive
+- [ ] Add site `<title>`
+
+  **Number Guessing**
+
+- [ ] Add 'too low' and 'too high' hints feature
+
+### Updates:
+
+- [ ] Update the window alert into a modal for contact form Thank you message
+- [ ] Align nav bar vertically and horizontally, both for desktop and mobile
+- [ ] Set uniform styling for page title in all pages
+- [ ] Reorganize the left Nav bar to Games, Leaderboard, Contact Us
+- [ ] Update the font colors globally - especially the pure black ones
+
+### Bug Fixing:
+
+**Nav Bar**
+
+- [ ] Fix file path to Contact Us page from Nav bar
+
+**BlackJack Game**
+
+- [ ] Set the arrow field to only allowing number between 1-5
+- [ ] Fix the content overlap
+- [ ] Remove 'Black Jack' from Games drop down list when it's currently on Black Jack' page
+
+**Number Guessing Game**
+
+- [ ] Update this instructions to reflect the game more accurately: _You'll provide hints like "too high" or "too low" until the game magically reads your mind and figures out your number._
+- [ ] Add function to reset Time remaining (to zero) when player loses
